ecd1b5f7565aa9c141aa600faf7cd1b8a0f16502
Tushar1401 <tushararora1401@gmail.com>
2020-11-02 15:44:02 +0530

    Updates

---
+++ precache-manifest.67703c3051c559e9e605466b6621e911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c18393bf8d651d5593104742a63220fa",
+    "url": "/ComplaintSection/index.html"
+  },
+  {
+    "revision": "2814c7fceb1453e038c3",
+    "url": "/ComplaintSection/static/css/main.06da7aba.chunk.css"
+  },
+  {
+    "revision": "9f84fb29d6307fe4e433",
+    "url": "/ComplaintSection/static/js/2.1ccc3800.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/ComplaintSection/static/js/2.1ccc3800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2814c7fceb1453e038c3",
+    "url": "/ComplaintSection/static/js/main.275dc1d8.chunk.js"
+  },
+  {
+    "revision": "cea08009a307d8fcc54c",
+    "url": "/ComplaintSection/static/js/runtime-main.db2da429.js"
+  }
+]);
